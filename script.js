65f0f9764b0c3458cab2a662037c67e505c0ef3d
imthe27 <fertrevi02@gmail.com>
2025-12-03 17:18:44 -0600

    added classes to manage the buttons, and the buttons to display logic

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let firstnumber = 0;
 let secondnumber = 0;
 let operator = "";
 let result = 0;
+const display = document.getElementById("display");
+const btns = document.querySelectorAll(".numbers");
 
 function add(firstnumber, secondnumber) {
     result = firstnumber + secondnumber;
@@ -42,3 +44,21 @@ function operate(firstnumber, secondnumber, operator) {
             break;
     }
 }
+
+Array.from(btns).forEach(button => {
+    button.addEventListener('click', function() {
+        if (display.textContent === "0") {
+            display.textContent = this.id;
+        } else {
+            if (this.id !== ".") {
+                display.textContent += this.id;
+            } else {
+                if (display.textContent.includes(".")) {
+                    console.log("error, punto puesto");
+                } else {
+                    display.textContent += this.id;
+                }
+            }
+        }
+    });
+});
